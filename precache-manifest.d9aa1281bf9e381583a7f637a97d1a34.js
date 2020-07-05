4bdf2c4a1925eceebb862ed0952f0e4cc0d0fa5b
YilunSun <osirisnero@gmail.com>
2020-07-04 22:25:17 -0700

    Updates

---
+++ precache-manifest.d9aa1281bf9e381583a7f637a97d1a34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4456e795e92615171f2d4e5b54cde0fb",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "d8249a7a815cc86bc3e8",
+    "url": "/playground/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91fc03981f33ebb931d2",
+    "url": "/playground/static/js/2.616451b4.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/playground/static/js/2.616451b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8249a7a815cc86bc3e8",
+    "url": "/playground/static/js/main.179a2f6e.chunk.js"
+  },
+  {
+    "revision": "895c97baf350dc475b41",
+    "url": "/playground/static/js/runtime-main.927a30d0.js"
+  },
+  {
+    "revision": "34dfeed20d644ba572bd2d8d31bc8d77",
+    "url": "/playground/static/media/contemplative-reptile.34dfeed2.jpg"
+  },
+  {
+    "revision": "1a994bebf90f51a841ce698652f7590f",
+    "url": "/playground/static/media/game_of_life.1a994beb.png"
+  },
+  {
+    "revision": "ed2be46e8af3e665b74b6b77268f68a4",
+    "url": "/playground/static/media/o.ed2be46e.png"
+  },
+  {
+    "revision": "373d9286b28b62360112fc109ca3d12c",
+    "url": "/playground/static/media/orereo.373d9286.gif"
+  },
+  {
+    "revision": "7d5b065174a2bc99826a6c6e97f37183",
+    "url": "/playground/static/media/re.7d5b0651.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.694e8cb7d00e2fcdd3357d620c05f339.js"
+  "/playground/precache-manifest.d9aa1281bf9e381583a7f637a97d1a34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/playground/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
