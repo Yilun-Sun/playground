9c8629adeb2bc3e903160cc54d421e221ca9b4f4
YilunSun <osirisnero@gmail.com>
2020-07-04 22:11:31 -0700

    v 0.1.1 UI change

--- src/components/pages/VisualAlgo/VisualAlgo.jsx
+++ src/components/pages/VisualAlgo/VisualAlgo.jsx
@@ -7,27 +7,37 @@ import Navigation from '../../topics/NavigationBar/NavigationBar';
 import paper, { Shape, Group, Point, PointText } from 'paper';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import Grid from '@material-ui/core/Grid';
 
 export default class VisualAlgo extends Component {
   constructor() {
     super();
-    this.state = { arrayLength: 50, animateSpeed: 200 };
+    this.state = { arrayLength: 30, animateSpeed: 100, steps: 0, algoName: '', showSnackBar: false };
     this.canvasBarGroup = undefined;
     this.canvasTextGroup = undefined;
     this.barArray = [];
     this.textVisibility = true;
     this.switchList = [];
-
     this.animatingColor = 'yellow';
+    this.alertContent = 'nothing';
+    this.alertSeverity = '';
   }
 
-  // 控制排序速度
-  // 开始排序
-  // 选择排序算法
-  // 设置数组长度
-  // 随机生成待排序数组
-  // 展示排序算法代码
-  // 显示当前所在代码行
+  // ❎ 控制排序速度
+  // ✅‍ 开始排序
+  // ✅ 选择排序算法
+  // ✅ 设置数组长度
+  // ✅ 随机生成待排序数组
+  // ❎ 展示排序算法代码
+  // ❎ 显示当前所在代码行
+  // ❎ 提示当前状态 例如完成 未完成 需要选择算法等
+  // ❎ 界面优化
 
   componentDidMount() {
     // setup paper
@@ -42,6 +52,12 @@ export default class VisualAlgo extends Component {
   }
 
   init = () => {
+    this.canvasBarGroup = undefined;
+    this.canvasTextGroup = undefined;
+    this.barArray = [];
+    this.switchList = [];
+    this.setState({ steps: 0 });
+
     const canvasWidth = this.canvas.width;
     const canvasHeight = this.canvas.height;
     const padding = 50;
@@ -55,12 +71,12 @@ export default class VisualAlgo extends Component {
     this.barArray = new Array(arrayLength);
 
     for (let i = 0; i < arrayLength; i++) {
-      const currentValue = Math.ceil((Math.random() * (canvasHeight - padding * 2)) / 5) * 5;
+      const currentValue = Math.round(Math.random() * 98 + 1);
       this.barArray[i] = currentValue;
       const path = new Shape.Rectangle({
         fillColor: 'white',
         bottomCenter: [padding + barWidth / 2 + barWidth * i * 2, canvasHeight - padding],
-        size: [barWidth, currentValue],
+        size: [barWidth, currentValue * ((canvasHeight - 2 * padding) / 100)],
         name: `bar-${i}`,
       });
       canvasBarGroup.addChild(path);
@@ -69,13 +85,17 @@ export default class VisualAlgo extends Component {
     // optional: text
     for (let i = 0; i < arrayLength; i++) {
       const text = new PointText(
-        new Point(padding + barWidth / 2 + barWidth * i * 2, canvasHeight - padding - this.barArray[i] - 10)
+        new Point(
+          padding + barWidth / 2 + barWidth * i * 2,
+          canvasHeight - padding - this.barArray[i] * ((canvasHeight - 2 * padding) / 100) - 10
+        )
       );
       text.justification = 'center';
       text.fillColor = 'white';
       text.content = this.barArray[i];
       text.name = `text-${i}`;
       text.visible = this.textVisibility;
+      text.fontSize = Math.max(barWidth, 8);
       canvasTextGroup.addChild(text);
     }
 
@@ -99,7 +119,7 @@ export default class VisualAlgo extends Component {
     console.log(this.barArray);
   };
 
-  handleSliderChange = (event, newValue) => {
+  handleLengthChange = (event, newValue) => {
     if (newValue !== this.state.arrayLength) {
       this.setState({ arrayLength: newValue });
       this.canvasBarGroup = undefined;
@@ -110,19 +130,48 @@ export default class VisualAlgo extends Component {
     }
   };
 
+  // TODO:
+  handleSpeedChange = (event, newValue) => {
+    if (newValue !== this.state.animateSpeed) {
+      this.setState({ animateSpeed: newValue });
+    }
+  };
+
   runAlgo = () => {
     // clear render list
     this.switchList = [];
 
     // this.bubbleSort(this.barArray);
-    this.quickSort(this.barArray, 0, this.barArray.length - 1);
+    // this.quickSort(this.barArray, 0, this.barArray.length - 1);
+    // this.selectionSort(this.barArray);
+    // this.insertionSort(this.barArray);
+
+    switch (this.state.algoName) {
+      case 'bubble':
+        this.bubbleSort(this.barArray);
+        break;
+      case 'quick':
+        this.quickSort(this.barArray, 0, this.barArray.length - 1);
+        break;
+      case 'selection':
+        this.selectionSort(this.barArray);
+        break;
+      case 'insertion':
+        this.insertionSort(this.barArray);
+        break;
+
+      default:
+        this.showMessage('Please select an algorithm first', 'warning');
+        return;
+    }
 
     // render
     this.animateAlgo();
   };
 
-  switchTwoBar = (index1, index2) => {
-    console.log('switch two bar');
+  switchTwoBar = (index1, index2, animateSpeed) => {
+    const steps = this.state.steps;
+    this.setState({ steps: steps + 1 });
 
     const canvasBarGroup = this.canvasBarGroup;
     const canvasTextGroup = this.canvasTextGroup;
@@ -145,7 +194,7 @@ export default class VisualAlgo extends Component {
       },
       {
         easing: 'easeInOutCubic',
-        duration: 20000 / this.state.animateSpeed,
+        duration: 20000 / animateSpeed,
       }
     );
     path2.tween(
@@ -154,7 +203,7 @@ export default class VisualAlgo extends Component {
       },
       {
         easing: 'easeInOutCubic',
-        duration: 20000 / this.state.animateSpeed,
+        duration: 20000 / animateSpeed,
       }
     );
     path1.tween(
@@ -182,7 +231,7 @@ export default class VisualAlgo extends Component {
       },
       {
         easing: 'easeInOutCubic',
-        duration: 20000 / this.state.animateSpeed,
+        duration: 20000 / animateSpeed,
       }
     );
     text2.tween(
@@ -191,7 +240,7 @@ export default class VisualAlgo extends Component {
       },
       {
         easing: 'easeInOutCubic',
-        duration: 20000 / this.state.animateSpeed,
+        duration: 20000 / animateSpeed,
       }
     );
 
@@ -204,13 +253,17 @@ export default class VisualAlgo extends Component {
   };
 
   animateAlgo = () => {
+    const animateSpeed = this.state.animateSpeed;
+
     if (this.switchList.length > 0) {
       const indexs = this.switchList.shift();
-      this.switchTwoBar(indexs[0], indexs[1]);
+      this.switchTwoBar(indexs[0], indexs[1], animateSpeed);
     }
 
     if (this.switchList.length > 0) {
-      setTimeout(() => this.animateAlgo(), 20000 / this.state.animateSpeed + 30);
+      setTimeout(() => this.animateAlgo(), 20000 / animateSpeed + 30);
+    } else {
+      this.showMessage('Sort done!', 'success');
     }
   };
 
@@ -277,13 +330,130 @@ export default class VisualAlgo extends Component {
     return items;
   }
 
-  // ??? sort
+  // selection sort
+  selectionSort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      let min = i;
+      for (let j = i + 1; j < len; j++) {
+        if (arr[min] > arr[j]) {
+          min = j;
+        }
+      }
+      if (min !== i) {
+        let tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+        this.switchList.push([i, min]);
+      }
+    }
+    // return arr;
+  };
+
+  // insertion sort
+  insertionSort = (inputArr) => {
+    let length = inputArr.length;
+    for (let i = 1; i < length; i++) {
+      let key = inputArr[i];
+      let j = i - 1;
+      while (j >= 0 && inputArr[j] > key) {
+        inputArr[j + 1] = inputArr[j];
+        this.switchList.push([j, j + 1]);
+        j = j - 1;
+      }
+      inputArr[j + 1] = key;
+    }
+    // return inputArr;
+  };
 
   // ??? sort
 
-  // ??? sort
+  handleAlgoChange = (event) => {
+    this.setState({ algoName: event.target.value });
+  };
+
+  handleSnackBarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ showSnackBar: false });
+  };
+
+  /**
+   *
+   * @param {string} msg
+   * @param {string} severity
+   */
+  showMessage = (msg, severity) => {
+    this.alertContent = msg;
+    this.alertSeverity = severity;
+    this.setState({ showSnackBar: true });
+  };
 
   render() {
+    const lengthMarks = [
+      {
+        value: 10,
+        label: '10',
+      },
+      {
+        value: 20,
+      },
+      {
+        value: 30,
+      },
+      {
+        value: 40,
+      },
+      {
+        value: 50,
+        label: '50',
+      },
+      {
+        value: 60,
+      },
+      {
+        value: 70,
+      },
+      {
+        value: 80,
+      },
+      {
+        value: 90,
+      },
+      {
+        value: 100,
+        label: '100',
+      },
+    ];
+
+    const speedMarks = [
+      {
+        value: 50,
+        label: '1/2',
+      },
+      {
+        value: 100,
+        label: '1',
+      },
+      {
+        value: 200,
+        label: '2',
+      },
+      {
+        value: 250,
+      },
+      {
+        value: 300,
+        label: '3',
+      },
+      {
+        value: 400,
+        label: '4',
+      },
+    ];
+
     return (
       <StyledComponent styleMap={VisualAlgoStyle}>
         {(useStyles) => {
@@ -302,24 +472,98 @@ export default class VisualAlgo extends Component {
                 />
               </div>
               <div className={classes.button_group}>
-                <Button onClick={() => this.runAlgo()}>Run Algo</Button>
-                <Button onClick={() => this.showGroup()}>show group</Button>
-                <Button onClick={() => this.toggleText()}>Toggle Text</Button>
-                <div style={{ padding: '0px 0px 0px 10px', width: '160px' }}>
-                  <Typography id="discrete-slider" gutterBottom>
-                    Array Length
-                  </Typography>
-                  <Slider
-                    value={typeof this.state.arrayLength === 'number' ? this.state.arrayLength : 0}
-                    onChange={this.handleSliderChange}
-                    aria-labelledby="discrete-slider"
-                    valueLabelDisplay="auto"
-                    step={10}
-                    marks
-                    min={10}
-                    max={100}
-                  />
-                </div>
+                <Grid container spacing={1} className={classes.grid_container}>
+                  <Grid item xs={4}>
+                    <FormControl variant="outlined">
+                      <InputLabel id="demo-simple-select-outlined-label">Select Algorithms</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-outlined-label"
+                        id="demo-simple-select-outlined"
+                        value={this.state.algoName}
+                        onChange={this.handleAlgoChange}
+                        label="Age"
+                        className={classes.formControl}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={'bubble'}>Bubble Sort</MenuItem>
+                        <MenuItem value={'quick'}>Quick Sort</MenuItem>
+                        <MenuItem value={'selection'}>Selection Sort</MenuItem>
+                        <MenuItem value={'insertion'}>Insertion Sort</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={5}></Grid>
+                  <Grid item xs={3}>
+                    <div>switch: {this.state.steps} times</div>
+                  </Grid>
+
+                  <Grid item xs={2}>
+                    <Typography gutterBottom>Array Length</Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Slider
+                      style={{ width: '160px' }}
+                      value={typeof this.state.arrayLength === 'number' ? this.state.arrayLength : 0}
+                      onChange={this.handleLengthChange}
+                      aria-labelledby="discrete-slider"
+                      valueLabelDisplay="auto"
+                      step={10}
+                      marks={lengthMarks}
+                      min={10}
+                      max={100}
+                    />
+                  </Grid>
+                  <Grid item xs={5}></Grid>
+                  <Grid item xs={3}>
+                    <Button
+                      className={classes.button}
+                      variant="contained"
+                      color="primary"
+                      disableElevation
+                      onClick={() => this.toggleText()}
+                    >
+                      Toggle Text
+                    </Button>
+                  </Grid>
+
+                  <Grid item xs={2}>
+                    <Typography gutterBottom>Animation Speed</Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Slider
+                      style={{ width: '160px' }}
+                      value={typeof this.state.animateSpeed === 'number' ? this.state.animateSpeed : 0}
+                      onChange={this.handleSpeedChange}
+                      valueLabelDisplay="auto"
+                      marks={speedMarks}
+                      aria-labelledby="discrete-slider-restrict"
+                      step={50}
+                      min={50}
+                      max={400}
+                    />
+                  </Grid>
+
+                  <Grid item xs={5}></Grid>
+                  <Grid item xs={3}>
+                    <Button
+                      className={classes.run_button}
+                      variant="contained"
+                      color="primary"
+                      disableElevation
+                      onClick={() => this.runAlgo()}
+                    >
+                      Run Algo
+                    </Button>
+                  </Grid>
+                </Grid>
+
+                <Snackbar open={this.state.showSnackBar} autoHideDuration={3000} onClose={this.handleSnackBarClose}>
+                  <Alert onClose={this.handleSnackBarClose} severity={this.alertSeverity}>
+                    {this.alertContent}
+                  </Alert>
+                </Snackbar>
               </div>
             </div>
           );
@@ -327,4 +571,8 @@ export default class VisualAlgo extends Component {
       </StyledComponent>
     );
   }
+}
+
+function Alert(props) {
+  return <MuiAlert id="alert" elevation={6} variant="filled" {...props} />;
 }
--- src/components/pages/VisualAlgo/VisualAlgoStyle.js
+++ src/components/pages/VisualAlgo/VisualAlgoStyle.js
@@ -1,4 +1,5 @@
 const defaultBackgroundColor = '#3e5780';
+const gridHeight = 50
 
 const styles = {
     main: {
@@ -17,19 +18,39 @@ const styles = {
         position: 'absolute',
         left: '200px',
         right: '200px',
-        height: '120px',
-        bottom: '50px',
-        backgroundColor: '#6a83ad',
-        borderRadius: '20px'
+        height: '200px',
+        bottom: '30px',
+        backgroundColor: '#f0f6ff',
+        borderRadius: '15px',
+        padding: '10px',
+    },
+    button: {
+        height: `${gridHeight - 10}px`,
+        width: '200px',
+    },
+    run_button: {
+        height: `${gridHeight - 10}px`,
+        width: '200px',
+        background: 'linear-gradient(45deg, #00e600 30%, #00e600 90%)',
     },
     main_canvas: {
         position: 'absolute',
         left: '200px',
         right: '200px',
         top: '100px',
-        bottom: '200px',
+        bottom: '260px',
         backgroundColor: '#3e5780',
     },
+    formControl: {
+        height: `${gridHeight}px`,
+        minWidth: '300px'
+    },
+    grid_container: {
+        // position: 'absolute',
+        // top: '30px',
+        // left: '30px'
+        padding: '15px'
+    }
 };
 
 export default styles;
